c7474a743aab1bca2a2c9ad76e84b9eba64ef49b
Vips120 <vipulsingh0000@gmail.com>
2021-07-12 19:05:51 +0530

    form

---
+++ project6/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Form from './form';
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <Form />
+      </React.Fragment>
+    );
+  }
+};
+
+export default App;
